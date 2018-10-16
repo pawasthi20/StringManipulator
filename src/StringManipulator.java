5d73ab5e6f68767a7c2975475c43b4484ea28652
pawasthi20 <43074073+pawasthi20@users.noreply.github.com>
2018-10-16 13:41:12 -0400

    Add files via upload

---
+++ src/StringManipulator.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @author pawasthi20
+ *
+ */
+public class StringManipulator 
+{	
+	
+	//Methods
+	
+	public String noVowels(String a)
+	{
+		for(int i = 0; i < a.length(); i++)
+		{
+			char ch = a.charAt(i);
+			
+			
+		}
+		String str = "";
+		str += 
+	}
+	
+}
